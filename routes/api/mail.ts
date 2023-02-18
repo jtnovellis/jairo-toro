5f29ffd3f4a795d20861cc0760faf1f4a2c74ac5
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-18 01:52:19 -0500

    change to sendgrid

--- routes/api/mail.ts
+++ routes/api/mail.ts
@@ -1,45 +1,33 @@
 import { Handlers, Status } from "$fresh/server.ts";
-import { SMTPClient } from "https://deno.land/x/denomailer@1.6.0/mod.ts";
+import { sendSimpleMail } from "https://deno.land/x/sendgrid@0.0.3/mod.ts";
 
 export const handler: Handlers = {
   async POST(request: Request) {
-    console.log(
-      Deno.env.get("HOSTNAME"),
-      Deno.env.get("USERNAME"),
-      Deno.env.get("PASSWORD"),
-    );
-    console.log(Deno.env.get("FROM"), Deno.env.get("TO"));
-    const client = new SMTPClient({
-      connection: {
-        hostname: Deno.env.get("HOSTNAME")!,
-        port: 465,
-        tls: true,
-        auth: {
-          username: Deno.env.get("USERNAME")!,
-          password: Deno.env.get("PASSWORD")!,
-        },
-      },
-    });
-
     const payload: { mail: string; message: string } | undefined = await request
       .json();
 
-    if (payload) {
-      try {
-        await client.send({
-          from: Deno.env.get("from")!,
-          to: Deno.env.get("to")!,
-          subject: `New message from ${payload.mail}`,
-          content: payload.message,
-        });
-        await client.close();
-        return new Response("", { status: Status.OK });
-      } catch {
-        return new Response("Error with mailing", {
-          status: Status.BadRequest,
-        });
-      }
+    if (!payload) {
+      return new Response("Bad Request", { status: Status.BadRequest });
     }
+    await sendSimpleMail({
+      subject: "New message from your website",
+      to: [{ email: Deno.env.get("TO")! }],
+      from: { email: Deno.env.get("FROM")! },
+      content: [
+        {
+          type: "text/plain",
+          value: `Mail: ${payload?.mail}\nMessage: ${payload?.message}`,
+        },
+        {
+          type: "text/html",
+          value:
+            `<p>Mail: ${payload?.mail}</p><p>Message: ${payload?.message}</p>`,
+        },
+      ],
+    }, {
+      apiKey: Deno.env.get("SENDGRID_API_KEY")!,
+    });
+
     return new Response("OK", { status: Status.OK });
   },
 };
