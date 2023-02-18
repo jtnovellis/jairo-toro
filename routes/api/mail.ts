a516cb87a05d4ec10af760006297ae6c21250ad7
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-02-18 02:06:50 -0500

    add code to catch error in mailing function

--- routes/api/mail.ts
+++ routes/api/mail.ts
@@ -9,25 +9,28 @@ export const handler: Handlers = {
     if (!payload) {
       return new Response("Bad Request", { status: Status.BadRequest });
     }
-    await sendSimpleMail({
-      subject: "New message from your website",
-      to: [{ email: Deno.env.get("TO")! }],
-      from: { email: Deno.env.get("FROM")! },
-      content: [
-        {
-          type: "text/plain",
-          value: `Mail: ${payload?.mail}\nMessage: ${payload?.message}`,
-        },
-        {
-          type: "text/html",
-          value:
-            `<p>Mail: ${payload?.mail}</p><p>Message: ${payload?.message}</p>`,
-        },
-      ],
-    }, {
-      apiKey: Deno.env.get("SENDGRID_API_KEY")!,
-    });
-
-    return new Response("OK", { status: Status.OK });
+    try {
+      await sendSimpleMail({
+        subject: "New message from your website",
+        to: [{ email: Deno.env.get("TO")! }],
+        from: { email: Deno.env.get("FROM")! },
+        content: [
+          {
+            type: "text/plain",
+            value: `Mail: ${payload?.mail}\nMessage: ${payload?.message}`,
+          },
+          {
+            type: "text/html",
+            value:
+              `<p>Mail: ${payload?.mail}</p><p>Message: ${payload?.message}</p>`,
+          },
+        ],
+      }, {
+        apiKey: Deno.env.get("SENDGRID_API_KEY")!,
+      });
+      return new Response("OK", { status: Status.OK });
+    } catch {
+      return new Response("Error", { status: Status.InternalServerError });
+    }
   },
 };
